80c564d494138922c18235aaf8284d295bf48536
Kwaku Ofosu <kbofosutuffour@gmail.com>
2024-03-23 16:55:23 -0400

    battle screen checkpoint

--- angular-frontend/src/app/battle/battle.component.ts
+++ angular-frontend/src/app/battle/battle.component.ts
@@ -10,6 +10,8 @@ import axios from 'axios';
 })
 export class BattleComponent {
   @Output() setView = new EventEmitter<boolean>();
+  left!: string;
+  right!: string;
 
   changeView(isHome: boolean) {
     this.setView.emit(isHome);
@@ -20,11 +22,17 @@ export class BattleComponent {
   }
 
   getImages() {
-    let character;
-    axios.get('')
+
+    axios.get('http://127.0.0.1:5000/Naruto_Uzumaki')
     .then((res) => {
-      character = res.data.images[0]
+      this.left = "https://static.wikia.nocookie.net/naruto/images/d/d6/Naruto_Part_I.png"
     })
     .catch((err) => console.log(err));
+    axios.get('http://127.0.0.1:5000/Sasuke_Uchiha')
+    .then((res) => {
+      this.right = "https://static.wikia.nocookie.net/naruto/images/2/21/Sasuke_Part_1.png"
+    })
+    .catch((err) => console.log(err));
+
   }
 }
--- flask-backend/main.py
+++ flask-backend/main.py
@@ -3,8 +3,10 @@ from flask_restful import Resource, Api
 import requests
 from bs4 import BeautifulSoup
 import sys
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 def getURL(name):
