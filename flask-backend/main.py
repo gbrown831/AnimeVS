4a18a025ab6baa32c65366779dd1da6e09e98ddb
AlanGonzalez10123 <alangonzalezosorio04@gmail.com>
2024-03-23 15:35:02 -0400

    added a function to get Naruto image URLs

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,8 +1,35 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import requests
+from bs4 import BeautifulSoup
+import sys
 
 app = Flask(__name__)
 api = Api(app)
+
+def getURL(url):
+    url = url       #'https://naruto.fandom.com/wiki/Naruto_Uzumaki'
+
+    url_list = []
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Set default encoding to UTF-8
+    sys.stdout.reconfigure(encoding='utf-8')
+
+    specific_class = soup.find_all(class_='smw-table-cell smwprops')
+
+    # Iterate over each element with the specific class name
+    for element in specific_class:
+        # Find all <a> elements inside the current element
+        a_elements_inside_class = element.find_all('a')
+        
+        # Print the href attribute of each <a> element found
+        for a_element in a_elements_inside_class:
+            if a_element['href'].startswith('http'):
+                url_list.append(a_element['href'])
+
+    return {"images": url_list}
 
 class HelloWorld(Resource):
     def get(self):
